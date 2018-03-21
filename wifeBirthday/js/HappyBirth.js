ffbf7cb4f4208dc0687eca99382cfb89d50d7d8f
Yas <yuanshi@anne.com.cn>
2018-03-21 14:30:20 +0800

    change time

--- wifeBirthday/js/HappyBirth.js
+++ wifeBirthday/js/HappyBirth.js
@@ -39,7 +39,7 @@ var Qixi = function() {
     }
     setInterval(function(){
         confi.picShow=0
-    },2000)
+    },500)
     if (confi.keepZoomRatio) {
         var proportionY = 900 / 1440;
         var screenHeight = $(document).height();
